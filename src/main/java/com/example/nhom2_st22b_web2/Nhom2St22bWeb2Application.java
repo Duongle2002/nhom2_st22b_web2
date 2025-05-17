830999b7b01b11d2c097068358f754d76b8b9ad7
huudu <duong100008@donga.edu.vn>
2025-05-17 14:14:42 +0700

    first new app

---
+++ src/main/java/com/example/nhom2_st22b_web2/Nhom2St22bWeb2Application.java
@@ -0,0 +1,13 @@
+package com.example.nhom2_st22b_web2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nhom2St22bWeb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nhom2St22bWeb2Application.class, args);
+	}
+
+}
