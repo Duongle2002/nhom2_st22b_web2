edc80ea5ad625d8055983e8f4cf5dd7c1b17c4a3
thanh ho <hothanh10062004@gmail.com>
2025-05-24 15:01:02 +0700

    rename memeberrepository

--- src/main/java/com/example/nhom2_st22b_web2/repositorys/MemberRepository.java
+++ src/main/java/com/example/nhom2_st22b_web2/repositorys/MemberRepository.java
@@ -4,10 +4,6 @@ package com.example.nhom2_st22b_web2.repositorys;
 import com.example.nhom2_st22b_web2.models.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member , Long> {
-import com.example.nhom2_st22b_web2.models.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 }
--- src/main/java/com/example/nhom2_st22b_web2/services/MemberService.java
+++ src/main/java/com/example/nhom2_st22b_web2/services/MemberService.java
@@ -2,7 +2,7 @@ package com.example.nhom2_st22b_web2.services;
 
 import com.example.nhom2_st22b_web2.models.Member;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.example.nhom2_st22b_web2.repositorys.MemberRepositorys;
+import com.example.nhom2_st22b_web2.repositorys.MemberRepository;
 import com.example.nhom2_st22b_web2.repositorys.*;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +12,7 @@ import java.util.List;
 public class MemberService {
 
     @Autowired
-    private MemberRepositorys memberRepository;
+    private MemberRepository memberRepository;
 
     public List<Member> getAllMembers() {
         return memberRepository.findAll();
